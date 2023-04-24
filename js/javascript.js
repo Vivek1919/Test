cee39d90910a398f6f07d7b7ac138a7b394ef6b3
Vivek1919 <99400801+Vivek1919@users.noreply.github.com>
2023-04-24 16:09:00 +0100

    update_V2

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,3 @@
-// We want to see some non-trivial Javascript code
-// At a minimum, you should demonstrate a few simple uses of event-driven JavaScript for DOM manipulation
-// You should use ES6 syntax throughout: e.g. don't use "var", use the modern tools (template literals, arrow functions).
-// There should be no JavaScript errors in the browser console
-
-// For more marks, we like to see a bit more complex use of JavaScript, perhaps some looping and/or more complex DOM manipulation.
-// Accessing APIs is a great option if it fits with your project, or you can work with your own, local data.
-// Your code should be DRY, if you have repeated code, consider refactoring as a function with arguments for example.
-// We like to see what you can do. Be creative.
 
 const prevButton = document.querySelector('#prev-btn');
 const nextButton = document.querySelector('#next-btn');
@@ -32,85 +23,110 @@ prevButton.addEventListener('click', () => {
 setInterval(() => {
   currentIndex = (currentIndex + 1) % galleryImgs.length;
   transitionImage();
-}, 8000);
+}, 3000);
 
+// Define a function to transition between gallery images
 function transitionImage() {
-    const prevIndex = (currentIndex - 1 + galleryImgs.length) % galleryImgs.length;
-    const nextIndex = (currentIndex + 1) % galleryImgs.length;
-    galleryImgs.forEach((img) => {
-      img.classList.remove('prev', 'active', 'next');
-    });
-    galleryImgs[prevIndex].classList.add('prev');
-    galleryImgs[currentIndex].classList.add('active');
-    galleryImgs[nextIndex].classList.add('next');
-  }
-  
+  // Calculate the index of the previous and next images in the gallery
+  const prevIndex = (currentIndex - 1 + galleryImgs.length) % galleryImgs.length;
+  const nextIndex = (currentIndex + 1) % galleryImgs.length;
+
+  // Remove the 'prev', 'active', and 'next' classes from all gallery images
+  galleryImgs.forEach((img) => {
+    img.classList.remove('prev', 'active', 'next');
+  });
+
+  // Add the 'prev' class to the previous image, 'active' to the current image, and 'next' to the next image
+  galleryImgs[prevIndex].classList.add('prev');
+  galleryImgs[currentIndex].classList.add('active');
+  galleryImgs[nextIndex].classList.add('next');
+}
+
+// Add a scroll event listener to the window to update the active navigation item based on the current section
 window.addEventListener("scroll", () => {
-    const sections = document.querySelectorAll("section");
-    const scrollY = window.pageYOffset;
+  const sections = document.querySelectorAll("section");
+  const scrollY = window.pageYOffset;
 
-    sections.forEach(current => {
-        let sectionHeight = current.offsetHeight;
-        let sectionTop = current.offsetTop - 50;
-        let id = current.getAttribute("id");
+  // Loop through each section to determine which one is currently in view
+  sections.forEach(current => {
+    let sectionHeight = current.offsetHeight;
+    let sectionTop = current.offsetTop - 50;
+    let id = current.getAttribute("id");
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
-        }
-        else {
-            document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active");
-
-        }
-    });
-
+    // If the current section is in view, add the 'active' class to the corresponding navigation item
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+    }
+    // Otherwise, remove the 'active' class from the corresponding navigation item
+    else {
+      document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active");
+    }
+  });
 });
 
+// Get references to the menu button, close button, navigation container, and navigation items
 const menuBtn = document.querySelector(".nav-menu-btn");
 const closeBtn = document.querySelector(".nav-close-btn");
 const navigation = document.querySelector(".navigation");
 const navItems = document.querySelectorAll(".nav-items a");
 
-
+// Add a click event listener to the menu button to open the navigation container
 menuBtn.addEventListener("click", () => {
-    navigation.classList.add("active");
+  navigation.classList.add("active");
 });
 
+// Add a click event listener to the close button to close the navigation container
 closeBtn.addEventListener("click", () => {
-    navigation.classList.remove("active");
+  navigation.classList.remove("active");
 });
 
+// Add a click event listener to each navigation item to close the navigation container when a navigation item is clicked
 navItems.forEach((navItem) => {
-    navItem.addEventListener("click", () => {
-        navigation.classList.remove("active");
-    });
+  navItem.addEventListener("click", () => {
+    navigation.classList.remove("active");
+  });
 });
 
-
+// Wait for the DOM content to load before executing the code
 document.addEventListener("DOMContentLoaded", function() {
   const track = document.getElementById("image-track");
 
+  // Add a mousemove event listener to the track element
   track.addEventListener('mousemove', function(e) {
+    // Get all HTML elements with a class of "image" inside the track element
     const images = track.getElementsByClassName("image");
+    // Calculate the maximum delta value for the mouse movement based on the window width
     const maxDelta = window.innerWidth * 0.9;
+    // Calculate the endpoint for the mouse movement based on the number of images and a fixed value
     const endPoint = -((images.length - 1) * 25);
+    // Set a maximum percentage for the next image position
     const maxNextPercentage = 20;
 
+    // Loop through all the images and update their object position based on the mouse position
     for (let i = 0; i < images.length; i++) {
+      // Get the current image element in the loop
       const image = images[i];
+      // Get the bounding rectangle for the current image
       const imageRect = image.getBoundingClientRect();
+      // Calculate the mouse delta for the current image
       const mouseDelta = e.clientX - (imageRect.left + imageRect.width / 2);
+      // Calculate the percentage of the mouse delta relative to the maximum delta
       const percentage = (mouseDelta / maxDelta) * -100;
 
+      // Calculate the next percentage for the next image position
       let nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
 
+      // Ensure the next percentage is within bounds
       if (nextPercentage > 0) {
         nextPercentage = 0;
       } else if (nextPercentage < endPoint || nextPercentage < -maxNextPercentage) {
         nextPercentage = Math.max(endPoint, -maxNextPercentage);
       }
 
+      // Set the object position for the current image based on the next percentage
       image.style.objectPosition = `${nextPercentage + 100}% 100%`;
       
+      // Animate the current image to its new object position
       image.animate(
         { objectPosition: `${60 + nextPercentage}% center` },
         { duration: 200, fill: "forwards" }
@@ -118,6 +134,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 
+  // Add a mouseleave event listener to reset the object position for all images when the mouse leaves the track element
   track.addEventListener('mouseleave', function() {
     const images = track.getElementsByClassName("image");
     for (let i = 0; i < images.length; i++) {
@@ -126,6 +143,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 });
+
 
 
 
